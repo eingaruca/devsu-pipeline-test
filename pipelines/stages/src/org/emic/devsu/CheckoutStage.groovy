10b964c58096482c50b7b583f2126c3f310082d2
eingaruca <eldo16@gmail.com>
2024-03-26 12:34:08 +0100

    feat: Adding libraries in Jenkins and create pipeline folder

---
+++ pipelines/stages/src/org/emic/devsu/CheckoutStage.groovy
@@ -0,0 +1,8 @@
+packaget org.emic.devsu
+
+class CheckoutStage {
+
+    static void init (script) {
+        script.echo "[INFO] Estoy en checkoutStage"
+    }
+}
---
+++ pipelines/stages/vars/mainPipeline.groovy
@@ -0,0 +1,7 @@
+import org.emic.devsu
+
+def call (body) {
+    withFolderProperties {
+        echo "[INFO] Test mainPipeline.groovy"
+    }
+}
