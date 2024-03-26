3089dc0d6ffa2a1e64e7f7e023d4f68e2008d573
Eldo Ingaruca <eldo16@gmail.com>
2024-03-26 16:07:45 +0100

    fix: Fixing BuildStage

--- pipelines/stages/vars/buildExec.groovy
+++ pipelines/stages/vars/buildExec.groovy
@@ -7,6 +7,7 @@ def call() {
         def maven = docker.image(image)
 
         maven.inside {
+            java -version
             sh "mvn -f ${env.WORKSPACE}/pom.xml clean -U -B package -Dskip.unit.tests=true -Dmaven.test.skip=true -Dpmd.skip=true"
             log.info "BuildStage", "Fin Build mvn"
         }
