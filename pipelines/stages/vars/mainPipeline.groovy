f4fbbc5fdda45a2726bd4380fa8c31468dcf90f3
eingaruca <eldo16@gmail.com>
2024-03-26 12:46:14 +0100

    feat: Testing CheckoutStage.groovy

--- pipelines/stages/vars/mainPipeline.groovy
+++ pipelines/stages/vars/mainPipeline.groovy
@@ -3,5 +3,12 @@ import org.emic.devsu.*
 def call (body) {
     withFolderProperties {
         echo "[INFO] Test mainPipeline.groovy"
+
+
+        node {
+            stage ( "Checkout Stage" ) {
+                CheckoutStage.init(this)
+            }
+        }
     }
 }
