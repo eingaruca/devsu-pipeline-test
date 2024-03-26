96bf216e47e739f89f3ebd2f4e6d1acb95b730e1
Eldo Ingaruca <eldo16@gmail.com>
2024-03-26 15:58:58 +0100

    feat: Adding mbn clean package in BuildStage

--- pipelines/stages/vars/buildExec.groovy
+++ pipelines/stages/vars/buildExec.groovy
@@ -1,5 +1,14 @@
 def call() {
     withFolderProperties {
-        log.info("BuildExec", "Estoy en BuidExec.groovy")
+        log.info("BuildExec", "Building")
+        def image = "maven:latest"
+        log.info("BuildStage", "Workspace: ${env.WORKSPACE}")
+     
+        def maven = docker.image(image)
+
+        maven.inside {
+            sh "mvn -f ${env.WORKSPACE}/frontend_testing/bootstrap_menu/testingsb/pom.xml clean -U -B package -Dskip.unit.tests=true -Dmaven.test.skip=true -Dpmd.skip=true"
+            log.info "BuildStage", "Fin Build mvn"
+        }
     }
 }
