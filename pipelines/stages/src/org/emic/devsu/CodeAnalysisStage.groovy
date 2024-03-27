e6d20779410b2c4c0719606cc38112250faf635c
Eldo Ingaruca <eldo16@gmail.com>
2024-03-27 16:13:41 +0100

    fix: Error in CodeAnalysisStretegy

--- pipelines/stages/src/org/emic/devsu/CodeAnalysisStage.groovy
+++ pipelines/stages/src/org/emic/devsu/CodeAnalysisStage.groovy
@@ -2,9 +2,37 @@ package org.emic.devsu
 
 class CodeAnalysisStage {
 
-    static void init (script){
-        // code .trim() and lowercase else if default error if not in options
-        script.log.info "CodeAnalysis", "Starting"
-        script.codeAnalysisStrategy("springboot")
+    static void init (script, projectTypeVar){
+
+        script.utils.info "CodeAnalysisStage", "Starting Static Code Analysis"
+        // script.codeAnalysisStrategy("springboot")
+
+        switch (projectTypeVar) {
+            case 'springboot':
+                codeAnalysisSBStrategy(script)
+                break
+            case 'nodejs':
+                codeAnalysisNodejsStrategy(script)
+                break
+            case 'django':
+                codeAnalysisDjangoStrategy(script)
+                break
+            default:
+                script.utils.error  "CodeAnalysisStage", "No existe tipo Proyecto, elija springboot, nodejs o django"
+        }
+
+        script.utils.info "CodeAnalysisStage", "Finish Static Code Analysis"
+    }
+
+    private static void codeAnalysisSBStrategy(script) {
+        script.codeAnalysisSBStrategy()
+    }
+
+    private static void codeAnalysisNodejsStrategy(script) {
+        script.codeAnalysisNodejsStrategy()
+    }
+
+    private static void codeAnalysisDjangoStrategy(script) {
+        script.codeAnalysisDjangoStrategy()
     }
 }
