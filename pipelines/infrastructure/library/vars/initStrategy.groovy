6c55eb1d9cbcc15aae1f0b40b04a1450470a0e76
Eldo Ingaruca <eldo16@gmail.com>
2024-03-29 10:32:58 +0100

    feat: Developing pipeline GCP

--- pipelines/infrastructure/library/vars/initStrategy.groovy
+++ pipelines/infrastructure/library/vars/initStrategy.groovy
@@ -1,5 +1,11 @@
 def call () {
     withFolderProperties {
-        sh "terraform --version"
+        
+        def image = "hashicorp/terraform:1.8.0-rc1"
+        def terraform = docker.image(image)
+        // Execute commands inside Docker container
+        terraform.inside {
+            sh "terraform --version"
+        }
     }
 }
