f2ed2a4adf2046c904210713bac55e9caebf5e96
Eldo Ingaruca <eldo16@gmail.com>
2024-03-29 19:38:18 +0100

    feat: Starting deliveryStage

--- pipelines/stages/vars/deliverySBStrategy.groovy
+++ pipelines/stages/vars/deliverySBStrategy.groovy
@@ -16,6 +16,8 @@ def call() {
         cloudsdk.inside {
             withCredentials([file(credentialsId: 'gke_devsu2', variable: 'GKE_CREDENTIALS_FILE')]) {
                 sh "apk add kubectl"
+                sh "gcloud components install app-engine-java kubectl"
+                sh "gcloud components install gke-gcloud-auth-plugin"
                 sh "gcloud auth activate-service-account --key-file=${GKE_CREDENTIALS_FILE}"
                 sh "gcloud container clusters get-credentials devsu-cluster-test --zone europe-west1-b --project devsu-project"
                 sh "kubectl get pods"
