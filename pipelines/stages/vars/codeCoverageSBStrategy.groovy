7a6015443e62a33f45d97d86cbfaacc8b48ddb34
Eldo Ingaruca <eldo16@gmail.com>
2024-03-31 20:32:35 +0200

    feat: CodeCoverage Strategy

--- pipelines/stages/vars/codeCoverageSBStrategy.groovy
+++ pipelines/stages/vars/codeCoverageSBStrategy.groovy
@@ -5,18 +5,20 @@ def call() {
         def image = "maven:3.8.3-openjdk-17"
         def maven = docker.image(image)
         // // Execute commands inside Docker container
-        unstash name : 'build'
+        
         maven.inside {
             // sh "java -version"
+            unstash name : 'build'
             sh "mvn clean test jacoco:report"
+            jacoco (
+                execPattern: '**/build/jacoco/*.exec',
+                classPattern: '**/build/classes/java/main',
+                sourcePattern: '**/src/main'
+            )
         }
 
 
-        jacoco (
-            execPattern: '**/build/jacoco/*.exec',
-            classPattern: '**/build/classes/java/main',
-            sourcePattern: '**/src/main'
-        )
+        
         utils.info "CodeCoverageStage", "Finish Springboot Strategy"
     }
 }
