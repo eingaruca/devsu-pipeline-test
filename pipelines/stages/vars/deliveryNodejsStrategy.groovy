ac4ba9290cc5b0984be6e027bd540e383796e496
Eldo Ingaruca <eldo16@gmail.com>
2024-03-31 19:18:37 +0200

    feat: nodejs deploy

--- pipelines/stages/vars/deliveryNodejsStrategy.groovy
+++ pipelines/stages/vars/deliveryNodejsStrategy.groovy
@@ -1,23 +1,19 @@
 def call() {
     withFolderProperties{
         utils.info "deliverNodejsStrategy", "Node.js Strategy"
+
+        def GIT_COMMIT_HASH = sh(script: "git log -n 1 --pretty=format:'%H'", returnStdout: true).trim()[0..6]
+        echo "Git commit = ${GIT_COMMIT_HASH}"
+
         sh "docker login -u eingaruca -p dckr_pat_I1tZsF5F4aAq-mq9kZSmp3SaGXA"
-        sh "docker build -t demonodejs ."
-        sh "docker tag demonodejs eingaruca/nodejs:v1"
-        sh "docker push eingaruca/nodejs:v1"
 
-        sh "docker rmi demonodejs"
-        sh "docker rmi eingaruca/nodejs:v1"
+        sh "docker build -t demonodejs:${GIT_COMMIT_HASH} ."
+        sh "docker tag demonodejs:${GIT_COMMIT_HASH} eingaruca/nodejs:${GIT_COMMIT_HASH}"
+        sh "docker push eingaruca/nodejs:${GIT_COMMIT_HASH}"
 
+        sh "docker rmi demonodejs:${GIT_COMMIT_HASH}"
+        sh "docker rmi eingaruca/nodejs:${GIT_COMMIT_HASH}"
 
-
-
-        sh "docker build -t demonodejs ."
-        sh "docker tag demonodejs eingaruca/nodejs:v1.1"
-        sh "docker push eingaruca/nodejs:v1.1"
-
-        sh "docker rmi demonodejs"
-        sh "docker rmi eingaruca/nodejs:v1.1"
         // sh 'docker rmi $(docker images -f "dangling=true" -q )'
     }
 }
--- pipelines/stages/vars/deploySBStrategy.groovy
+++ pipelines/stages/vars/deploySBStrategy.groovy
@@ -9,6 +9,10 @@ def call() {
             withCredentials([file(credentialsId: 'gke_devsu2', variable: 'GKE_CREDENTIALS_FILE')]) {
                 sh "gcloud auth activate-service-account --key-file=${GKE_CREDENTIALS_FILE}"
                 sh "gcloud container clusters get-credentials devsu-cluster-test --zone europe-west1-b --project devsu-project"
+                def GIT_COMMIT_HASH = sh(script: "git log -n 1 --pretty=format:'%H'", returnStdout: true).trim()[0..6]
+                echo "Git commit = ${GIT_COMMIT_HASH}"
+                sh "sed -i 's|TAGVARIABLE|${GIT_COMMIT_HASH}|g' manifest.yaml"
+                sh "cat manifest.yaml"
                 sh "kubectl delete -f ./manifest.yaml --ignore-not-found"
                 sh "kubectl apply -f ./manifest.yaml"
                 sh "sleep 5"
