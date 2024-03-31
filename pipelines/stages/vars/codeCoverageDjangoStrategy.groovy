430a2e982e63012300f460d4aed64722d7216782
Eldo Ingaruca <eldo16@gmail.com>
2024-03-31 20:54:44 +0200

    feat: CodeCoverage Python Strategy

--- pipelines/stages/vars/codeCoverageDjangoStrategy.groovy
+++ pipelines/stages/vars/codeCoverageDjangoStrategy.groovy
@@ -1,5 +1,24 @@
 def call() {
     withFolderProperties{
         utils.info "CodeCoverageStage", "Django Strategy"
+        // Define Docker image
+        def image = "python:alpine3.19"
+        def python = docker.image(image)
+
+        // Execute commands inside Docker container
+        python.inside {
+            unstash name: 'build'
+            // sh "pip install -r requirements.txt"
+            sh "pip install coverage"
+            sh "python manage.py migrate"
+
+            // Ejecutar pruebas con cobertura
+            sh "coverage run manage.py test"
+            
+            // Generar informe de cobertura
+            sh "coverage report"
+            sh "coverage html"
+        }
+
+        utils.info "CodeCoverageStage", "Finish Django Strategy"
     }
-}
