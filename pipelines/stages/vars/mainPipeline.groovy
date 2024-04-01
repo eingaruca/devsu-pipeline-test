58c30e7f46d50cbc186921de1e480826e09a8395
eingaruca <eldo16@gmail.com>
2024-04-01 14:23:46 +0200

    docs: Documentación

--- pipelines/stages/vars/mainPipeline.groovy
+++ pipelines/stages/vars/mainPipeline.groovy
@@ -31,24 +31,24 @@ def call (body) {
             // Optional Test Stage
             if ( testVar != null  ){
                 stage ( "Test Stage" ) {
-                    // TestStage.init(this, projectTypeVar, testVar)
-                    utils.info "mainPipeline", "Test STAGE"
+                    TestStage.init(this, projectTypeVar, testVar)
+                    // utils.info "mainPipeline", "Test STAGE"
                 }
             }
 
             // Optional Static Code Analysis Stage
             if ( codeAnVar != null || codeAnVar ){
                 stage ( "Static Code Analysis Stage" ){
-                    utils.info "mainPipeline", " Code Analysis STAGE"
-                    // CodeAnalysisStage.init(this, projectTypeVar)
+                    // utils.info "mainPipeline", " Code Analysis STAGE"
+                    CodeAnalysisStage.init(this, projectTypeVar)
                 }
             }
 
             // Optional Code Coverage Stage
             if ( codeCoverageVar ){
                 stage ( "Code Coverage Stage" ){
-                    utils.info "mainPipeline", " Code Coverage STAGE"
-                    // CodeCoverageStage.init(this, projectTypeVar)
+                    // utils.info "mainPipeline", " Code Coverage STAGE"
+                    CodeCoverageStage.init(this, projectTypeVar)
                 }
             }
 
